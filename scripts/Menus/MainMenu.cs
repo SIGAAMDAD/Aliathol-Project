07a26a617c2e1f951d76f49e4ca8a328974a74f9
Noah Van Til <nyvantil@gmail.com>
2025-10-26 09:32:19 -0500

    Initial commit

---
+++ scripts/Menus/MainMenu.cs
@@ -0,0 +1,32 @@
+using Godot;
+
+/*
+===================================================================================
+
+MainMenu
+
+===================================================================================
+*/
+/// <summary>
+/// 
+/// </summary>
+
+public sealed partial class MainMenu : Control {
+	private enum State : uint {
+		SaveSlots,
+		Settings,
+		Credits,
+	};
+
+	/*
+	===============
+	_Ready
+	===============
+	*/
+	/// <summary>
+	/// 
+	/// </summary>
+	public override void _Ready() {
+		
+	}
+};
